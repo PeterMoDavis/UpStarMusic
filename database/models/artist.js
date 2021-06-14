a2416866c1d8dc9c010af170f1195b4f13d36d7b
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-14 13:52:30 -0400

    added Schemas

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,24 @@
 // Todo: Create Artist Model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: String,
+  genre: String,
+  website: String,
+  newWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "album",
+    },
+  ],
+});
+
+const Artist = mongoose.model("artist", ArtistSchema);
+
+module.exports = Artist;

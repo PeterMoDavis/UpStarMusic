312a48ec71a28f26eef07d22133200ee084f166a
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-14 19:11:09 -0400

    added text search

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -12,7 +12,7 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
   //Write a query that will follow sort, offset, limit options only
   // do not worry about "criteria" yet
   console.log(criteria);
-  const query = Artist.find({})
+  const query = Artist.find(buildQuery(criteria))
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit);
@@ -25,4 +25,27 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
       limit: limit,
     };
   });
+};
+
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max,
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max,
+    };
+  }
+  return query;
 };
